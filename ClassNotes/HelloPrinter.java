58464e1315a6fc2c3aefe29412df78aadf20eeef
Geoff Schmit <gcschmit@gmail.com>
2017-08-22 12:23:06 -0500

    Started Chapter 1 Notes
    start tomorrow with methods

---
+++ ClassNotes/HelloPrinter.java
@@ -0,0 +1,34 @@
+
+/**
+ * Every Java program contains one or more classes (e.g., HelloPrinter)
+ * 
+ * In general, every source file (HelloPrinter.java) contains one
+ *      class (HelloPrinter).
+ *      
+ * The source file name must match class name.
+ * 
+ * By convention, class names start with an uppercase letter.
+ *
+ * @author gcschmit
+ * @version 22 August 2017
+ */
+public class HelloPrinter
+{
+    /*
+     * A class contains methods.
+     * 
+     * Every Java application contains a class with a main method.
+     * 
+     * The main method is executed when the application starts.
+     */
+    public static void main( String[] args )
+    {
+        
+    }
+}
+
+
+
+
+
+
