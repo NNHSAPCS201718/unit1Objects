14ccc2ea7320ebf3dad97ec21b886e823e3d5f93
Geoff Schmit <gcschmit@gmail.com>
2017-08-23 12:45:15 -0500

    More Chapter 1 Notes
    start with syntax errors

--- ClassNotes/HelloPrinter.java
+++ ClassNotes/HelloPrinter.java
@@ -23,6 +23,52 @@ public class HelloPrinter
      */
     public static void main( String[] args )
     {
+        /*
+         * A method contains statements.
+         * 
+         * Statements may invoke other methods (e.g., println).
+         * 
+         * In general, methods are invoke on objects (e.g., System.out).
+         * 
+         * When invoking a method, arguments are passed in parentheses
+         *      (e.g., "Hello, World!")
+         */
+        System.out.println( "Hello, World!" );  // comment at the end of the line
+        
+        // this is a single-line comment (like # python)
+        
+        /*
+         * Identifiers are any combination of:
+         *      letters
+         *      digits (but not as first character)
+         *      underscores
+         *      dollar signs
+         *      
+         * These are various identifiers:
+         */
+        int y, x2, x_y, x$;
+        
+        // This is not a valid identifier
+        //int 2y;
+        
+        /*
+         * Identifiers are case sensitive (case matters).
+         * 
+         * These are three different identifiers:
+         */
+        String box, BOX, Box;
+        
+        /*
+         * By convention, constants are all uppercase.
+         *      Use an underscore to separate words.
+         */
+        int SPEED_OF_LIGHT = 300000000;
+        
+        /*
+         * By convention, all other identifiers start with a lowercase letter.
+         *      Subsequent words start with an uppercase letter (CamalCase)
+         */
+        String firstName;
         
     }
 }
